3a30234b8c7e45723f83d9168b572d8a35db16ae
MohamedH1000 <122827442+MohamedH1000@users.noreply.github.com>
2023-10-18 23:36:19 +0300

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# high amounts file opened probelm to be fixed
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
